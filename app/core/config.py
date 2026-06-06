4c2927ab12e1add181e2f37906d29545633437a3
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2026-06-06 19:09:25 +0900

    feat: 헬스체크 추가 및 app 컨테이너 추가

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_PORT = int(os.getenv("DB_PORT", 3307))
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+# 실제 product 환경에서는 default 삭제
+DB_HOST = os.getenv("DB_HOST", "mysql")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER","root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_NAME = os.getenv("DB_NAME", "liveclass_logs")
